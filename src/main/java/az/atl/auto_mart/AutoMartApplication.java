f728baef27eab0cbf3d5d29526f87aba69008c2c
Əziz Eyvazov <ezizeyvazov313@mail.ru>
2024-10-07 11:15:55 +0400

    project created 07.10.2024

---
+++ src/main/java/az/atl/auto_mart/AutoMartApplication.java
@@ -0,0 +1,13 @@
+package az.atl.auto_mart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoMartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/atl/auto_mart/AutoMartApplicationTests.java
@@ -0,0 +1,13 @@
+package az.atl.auto_mart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
